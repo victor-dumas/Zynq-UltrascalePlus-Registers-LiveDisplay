df5e46b5add46a4bc0d2e84221e0d96e1f6ee76a
vdumas <victor.dumas@bertin.fr>
2022-05-22 22:56:43 +0200

    Add comments

--- server.py
+++ server.py
@@ -3,10 +3,14 @@ from bs4 import BeautifulSoup
 import copy
 import re
 
-
+'''
+@brief  Get bits from value using the selected mask (bits)
+        The masks has a VHDL syntax (e.g. 15:8 -> [8:16])
+'''
 def getBits(value, bits):
     result = 0
     splits = bits.split(':')
+    # Value to binary array
     bin_list = [int(x) for x in bin(value)[2:]]
     
     start = len(bin_list) - int(splits[0]) - 1
@@ -14,6 +18,7 @@ def getBits(value, bits):
     if len(splits) > 1:
         end = len(bin_list) - int(splits[1])
     
+    # Binary array to value (selecting only the masked bits)
     for bit in bin_list[start:end]:
         result = (result << 1) | bit
 
@@ -30,14 +35,11 @@ def genHtml(file):
         tables = soup.find_all('table')
         addrs_tables = []
 
-        reg_page = False
-        h2 = soup.find('h2')
-        if h2 is not None and "Register" in h2.string:
-            reg_page = True
-
         if len(tables) == 2:
+            # Get the base addresses for all registers for module pages and the register being dissected for a reg page
             addrs = tables[0].find_all('tr')[2].find('td').renderContents().decode('utf-8') .split('<br/>')
 
+            # Get the addresses and names of each register for a module
             for addr in addrs:
                 # Remove newlines
                 addr = addr.strip()
@@ -47,25 +49,32 @@ def genHtml(file):
                     new_table = copy.copy(tables[1])
                     tables[1].insert_after(new_table)
         else:
+            # If the page is not recognized as a module or register page then return the render of the origin HTML page
             return render_template(file)
 
-        print(addrs_tables)
+        # Record if the current page is a register page or a module page
+        reg_page = False
+        h2 = soup.find('h2')
+        if h2 is not None and "Register" in h2.string:
+            reg_page = True
         
         tables = soup.find_all('table')
         
         reg = 0
         for table in range (0, max(len(addrs_tables), 1)):
-
+            # For each line in the table
             lines = tables[table + 1].find_all('tr')
             for line in lines:
                 append_index = 2
                 i = 0
+                # Remove empty columns
                 for col in line:
                     if col == '\n':
                         append_index += 1
                     if i == append_index - 1:
                         reg = col.string
                     i += 1
+                # Value line
                 if len(line.find_all('td')) > 0:
                     selection = ""
                     if reg_page:
@@ -74,8 +83,11 @@ def genHtml(file):
                     else:
                         reg = int(reg, base = 16) + int(addrs_tables[table][0], base = 16)
                     line.insert(append_index, BeautifulSoup(f'''<td class='hex'>0x{reg:08X}''' + selection + '''</td>''', 'html.parser'))
+                # Header line
                 elif len(line.find_all('th')) > 0:
                     line.insert(append_index, BeautifulSoup(f'''<th >Value</th>''', 'html.parser'))
+        # Remove original table
+        tables[-1].extract()
         return render_template_string(str(soup))
 
 app = Flask(__name__)
